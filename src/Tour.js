3e1c1213bff9c59bcade5b5804280591b4b15118
Smit <ptlsmit.b@gmail.com>
2022-07-13 09:42:04 -0700

    Adding show more and read more functionality

--- src/Tour.js
+++ src/Tour.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = ({id,name,price,image,info}) => {
-  return <article className='single-tour'>
-    <img src={image} alt={name}/>
-    <footer>
-      <div className='tour-info'>
-        <h4>{name}</h4>
-        <h4 className='tour-price'>${price}</h4>
-      </div>
-      <p>{info}</p>
-      <button className='delete-btn'>Not Interested</button>
-    </footer>
-  </article>;
+const Tour = ({ id, name, price, image, info }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ?  info  : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button className="delete-btn">Not Interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
